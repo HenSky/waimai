c079a89cb091bbabf864d0d7b265ce7b945e8ef6
HenSky <1557677484121632@163.com>
2019-09-03 19:36:40 +0800

    整合了个人信息页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 // 路由器对象模块
 import Vue from 'vue'
 import Router from 'vue-router'
-import Msite from '../pages/Msite/Msite.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
-import Search from '../pages/Search/Search.vue'
-import Login from '../pages/Login/Login.vue'
-import Shop from '../pages/shop/shop.vue'
-import ShopFoods from '../pages/shop/shopFoods/shopFoods.vue'
-import ShopInfo from '../pages/shop/shopInfo/shopInfo.vue'
-import ShopPinjia from '../pages/shop/shopPinjia/shopPinjia.vue'
+import Msite from "../pages/Msite/Msite.vue";
+import Order from "../pages/Order/Order.vue";
+import Profile from "../pages/Profile/Profile.vue"
+import Search from "../pages/Search/Search.vue";
+import Login from "../pages/Login/Login.vue";
+import Shop from "../pages/shop/shop.vue";
+import ShopFoods from "../pages/shop/shopFoods/shopFoods.vue";
+import ShopInfo from "../pages/shop/shopInfo/shopInfo.vue";
+import ShopPinjia from "../pages/shop/shopPinjia/shopPinjia.vue";
 Vue.use(Router)
 
 export default new Router({
@@ -47,25 +47,6 @@ export default new Router({
       }
     },
     {
-<<<<<<< HEAD
-      path: '/',
-      redirect: '/msite'
-    },
-    {
-      path: '/login',
-      component: Login
-    },
-    {
-      path: '/shop/:id',
-      component: Shop,
-      children: [
-        { path: '/shop/foods', component: ShopFoods },
-        { path: '/shop/info', component: ShopInfo },
-        { path: '/shop/pinjia', component: ShopPinjia },
-        { path: '', redirect: '/shop/foods' }
-      ]
-    }
-=======
       path: "/login",
       component: Login
     },
@@ -77,6 +58,5 @@ export default new Router({
       path: "/",
       redirect: "/shop"
     },
->>>>>>> f53c158beca1e7403dbb48d218f043fd9777b7de
   ]
 })
--- src/vantPulugin/vantImport.js
+++ src/vantPulugin/vantImport.js
@@ -1,45 +1,36 @@
 import Vue from 'vue'
 
 import {
-<<<<<<< HEAD
-  Cell, CellGroup,
+  Cell,
+  CellGroup,
   Card,
-  Button,
   Tabbar,
-  TabbarItem
-} from 'vant'
-
-Vue
-  .use(Cell).use(CellGroup)
-  .use(Card)
-  .use(Button)
-  .use(Tabbar)
-  .use(TabbarItem)
-=======
-    Button,
-    Tabbar,
-    TabbarItem,
-    Icon,
-    Tab,
-    Tabs,
-    TreeSelect,
-    Field,
-    Stepper,
-    SubmitBar,
-    Popup,
-    NavBar
+  Button,
+  TabbarItem,
+  Icon,
+  Tab,
+  Tabs,
+  TreeSelect,
+  Field,
+  Stepper,
+  SubmitBar,
+  Popup,
+  NavBar
 } from "vant";
 
 Vue.use(Button)
-    .use(Tabbar)
-    .use(TabbarItem)
-    .use(Icon)
-    .use(Tab)
-    .use(Tabs)
-    .use(TreeSelect)
-    .use(Field)
-    .use(Stepper)
-    .use(SubmitBar)
-    .use(Popup)
-    .use(NavBar);
->>>>>>> f53c158beca1e7403dbb48d218f043fd9777b7de
+  .use(Icon)
+  .use(Tab)
+  .use(Tabs)
+  .use(TreeSelect)
+  .use(Field)
+  .use(Stepper)
+  .use(SubmitBar)
+  .use(Popup)
+  .use(NavBar)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Card)
+  .use(Button)
+  .use(Tabbar)
+  .use(TabbarItem);
