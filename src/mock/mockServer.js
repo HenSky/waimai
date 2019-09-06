200dfd82eb4505959e3c9da4c26f01426b8b0f98
Your Name <807216131@qq.com>
2019-09-06 19:25:30 +0800

    last

--- src/mock/mockServer.js
+++ src/mock/mockServer.js
@@ -1,6 +1,6 @@
-import Mock from "mockjs";
-import data from "./data.js/index.js";
+import Mock from 'mockjs'
+import data from './data.js/index.js'
 
-Mock.mock("/info", { code: 0, data: data.info });//获取商家信息
-Mock.mock("/goods", { code: 0, data: data.goods });//获取商家食物列表
-Mock.mock("/ratings", { code: 0, data: data.ratings });//获取商家评论
+Mock.mock('/info', { code: 0, data: data.info })// 获取商家信息
+Mock.mock('/goods', { code: 0, data: data.goods })// 获取商家食物列表
+Mock.mock('/ratings', { code: 0, data: data.ratings })// 获取商家评论
